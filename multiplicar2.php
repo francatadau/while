34d9f752c4812c21fee6a463b0991ab57a8d809f
forpi <francisco.ortola.piles@gmail.com>
2016-10-19 13:07:01 +0200

    while

---
+++ multiplicar2.php
@@ -0,0 +1,16 @@
+<?php
+//numero por el que vamos a multiplicar
+$numero=1;
+
+echo "Tabla de multiplicar del numero 2 <br>";
+
+while ($numero <= 10){
+
+  $resultado=$numero * 2;
+
+  echo "2 * $numero = $resultado <br>";
+
+  $numero++;
+}
+
+ ?>
